daff2dc300a8b7ba101a622599ba3c6c46b15973
Piotr Konsek <piotr.konsek@gmail.com>
2017-01-22 17:35:13 +0100

    merge two break anomalies searching method into one

--- src/main/java/pl/edu/agh/controllers/AnomalyController.java
+++ src/main/java/pl/edu/agh/controllers/AnomalyController.java
@@ -21,33 +21,7 @@ import java.util.List;
 public class AnomalyController extends CassandraTableScanBasedController {
     public final double TOLERANCE = 0.1;
 
-    /*
-        Przerwy w działaniu licznika, metodologia:
-        - Dane powinny występować do 90 sekund, jeżeli przerwa jest dłuższa niż ten czas( * tolerancja) to jest
-         to anomalia która trafia do raportu
-     */
-
-    /**
-     * @param sensorId
-     * @param fromDate - in seconds, inclusive
-     * @param toDate   - in seconds, exclusive
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.GET, value = "/sensors/{sensorId}/pause_anomalies")
-    public ResponseEntity<AnomalyReport> checkForPauseAnomalies(@PathVariable("sensorId") String sensorId, @RequestParam("from") Long fromDate, @RequestParam("to") Long toDate) {
-        AnomalyReport anomalyReport = new AnomalyReport();
-
-        List<SensorEntry> sensorEntryList = getEntryList(sensorId, fromDate, toDate);
-        List<Pair<Date, Date>> anomalies = getAnomalies(sensorEntryList);
-
-        anomalyReport.setEntries(sensorEntryList);
-        anomalyReport.setAnomaliesDates(anomalies);
-
-        return new ResponseEntity<AnomalyReport>(anomalyReport, HttpStatus.OK);
-    }
-
-
-    /*
+      /*
     Sprawdzanie anomalii - niesprawności czujnika
     Rozpatrywane w zadanym okresie czasu preferowalnie w kontekście danego dnia
     ---
@@ -79,30 +53,38 @@ public class AnomalyController extends CassandraTableScanBasedController {
     http://datapigtechnologies.com/blog/index.php/highlighting-outliers-in-your-data-with-the-tukey-method/
      */
 
+    /*
+        Przerwy w działaniu licznika, metodologia:
+        - Dane powinny występować do 90 sekund, jeżeli przerwa jest dłuższa niż ten czas( * tolerancja) to jest
+         to anomalia która trafia do raportu
+     */
+
     /**
      * @param sensorId
      * @param fromDate - in seconds, inclusive
      * @param toDate   - in seconds, exclusive
      * @return
      */
-    @RequestMapping(method = RequestMethod.GET, value = "/sensors/{sensorId}/anomalies")
-    public ResponseEntity<AnomalyReport> checkForAnomalies(@PathVariable("sensorId") String sensorId, @RequestParam("from") Long fromDate, @RequestParam("to") Long toDate) {
+    @RequestMapping(method = RequestMethod.GET, value = "/sensors/{sensorId}/pause_anomalies")
+    public ResponseEntity<AnomalyReport> checkForPauseAnomalies(@PathVariable("sensorId") String sensorId, @RequestParam("from") Long fromDate, @RequestParam("to") Long toDate) {
         AnomalyReport anomalyReport = new AnomalyReport();
 
         List<SensorEntry> sensorEntryList = getEntryList(sensorId, fromDate, toDate);
-        List<HourSensorEntryList> hourSensorEntryLists = splitByHours(sensorEntryList);
-        List<Date> anomalies = getAnomaliesTemp(hourSensorEntryLists);
+        List<Pair<Date, Date>> anomaliesRange = getAnomaliesPeterMethod(sensorEntryList);
+        List<Date> anomalies = getAnomaliesVictorMethod(sensorEntryList);
 
         anomalyReport.setEntries(sensorEntryList);
+        anomalyReport.setAnomaliesDates(anomaliesRange);
         anomalyReport.setAnomalies(anomalies);
 
         return new ResponseEntity<AnomalyReport>(anomalyReport, HttpStatus.OK);
     }
 
-    private List<Date> getAnomaliesTemp(List<HourSensorEntryList> hourSensorEntryLists) {
+    private List<Date> getAnomaliesVictorMethod(List<SensorEntry> sensorEntryList) {
         List<Date> anomalies = new ArrayList<>();
-
+        List<HourSensorEntryList> hourSensorEntryLists = splitByHours(sensorEntryList);
         DescriptiveStatistics stats = new DescriptiveStatistics();
+
         for (int i = 0; i < hourSensorEntryLists.size(); i++) {
             stats.addValue(hourSensorEntryLists.get(i).getSensorEntryList().size());
         }
@@ -115,6 +97,20 @@ public class AnomalyController extends CassandraTableScanBasedController {
         for (HourSensorEntryList hourSensorEntryList : hourSensorEntryLists) {
             if (hourSensorEntryList.getSensorEntryList().size() < lowerFence)
                 anomalies.add(hourSensorEntryList.getDate());
+        }
+
+        return anomalies;
+    }
+
+    private List<Pair<Date, Date>> getAnomaliesPeterMethod(List<SensorEntry> sensorEntryList) {
+        List<Pair<Date, Date>> anomalies = new ArrayList<>();
+
+        double standardTime = 90; // SECONDS
+        double toleranceTime = 90 + standardTime * TOLERANCE;
+
+        for (int i = 1; i < sensorEntryList.size(); i++) {
+            if (Math.abs(sensorEntryList.get(i).getTimestamp().getTime() - sensorEntryList.get(i).getTimestamp().getTime()) > toleranceTime)
+                anomalies.add(new Pair(sensorEntryList.get(i - 1).getTimestamp(), sensorEntryList.get(i).getTimestamp()));
         }
 
         return anomalies;
@@ -142,7 +138,6 @@ public class AnomalyController extends CassandraTableScanBasedController {
         return hourSensorEntryList;
     }
 
-
     private List<SensorEntry> getEntryList(String sensorId, Long fromDate, Long toDate) {
         List<SensorEntry> sensorEntryList;
         sensorEntryList = getMeasurementsBetween(sensorId, fromDate, toDate).map(
@@ -156,17 +151,4 @@ public class AnomalyController extends CassandraTableScanBasedController {
         return sensorEntryList;
     }
 
-    private List<Pair<Date, Date>> getAnomalies(List<SensorEntry> sensorEntryList) {
-        List<Pair<Date, Date>> anomalies = new ArrayList<>();
-
-        double standardTime = 90; // SECONDS
-        double toleranceTime = 90 + standardTime * TOLERANCE;
-
-        for (int i = 1; i < sensorEntryList.size(); i++) {
-            if (Math.abs(sensorEntryList.get(i).getTimestamp().getTime() - sensorEntryList.get(i).getTimestamp().getTime()) > toleranceTime)
-                anomalies.add(new Pair(sensorEntryList.get(i - 1).getTimestamp(), sensorEntryList.get(i).getTimestamp()));
-        }
-
-        return anomalies;
-    }
 }
