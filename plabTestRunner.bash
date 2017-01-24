422d664b455976faa67140b109e7a5b54d863ed5
Marcin Lachowicz <Marcin.Lachowicz@sabre.com>
2017-01-24 01:01:28 +0100

    up

--- plabTestRunner.bash
+++ plabTestRunner.bash
@@ -4,7 +4,17 @@ HTTP_METHOD=$1
 URL=$2
 ITERATIONS=$3
 
-./run.bash
+nohup ./run.bash > app.log 2>&1 &
+echo "`date` Waiting for application to start..."
+sleep 5
+
+grep_result=
+while [ -n "$grep_result" ]
+do
+    sleep 5
+    grep_result=`grep "Started Application in" app.log | grep -v grep`
+done
+
 echo "`date` Starting test for $HTTP_METHOD $URL"
 
 duration_sum=0
@@ -12,7 +22,7 @@ for i in `seq 1 $ITERATIONS`
 do
     echo "`date` Iteration $i"
     startTime=`perl -MTime::HiRes -e 'printf("%.0f\n",Time::HiRes::time()*1000)'`
-    curl -s -X $HTTP_METHOD "$URL" > /dev/null
+    curl -s -X $HTTP_METHOD "$URL"
     endTime=`perl -MTime::HiRes -e 'printf("%.0f\n",Time::HiRes::time()*1000)'`
     duration=$(expr $endTime - $startTime)
     echo "Response time: $duration [ms]"
